bc8d99ba1b71262a29213d668f530d9960aaf771
Gustavo Fortunato Puga <gustavo.fortunatopuga@gmail.com>
2013-04-13 17:08:41 -0300

    Teste para cobrir o cálculo do preco total de um produto comum.

---
+++ src/test/java/br/com/airu/produto/ProdutoComumTest.java
@@ -0,0 +1,26 @@
+package br.com.airu.produto;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProdutoComumTest {
+
+	private Produto produto;
+
+	@Before
+	public void criaProdutoComum() {
+		produto = new ProdutoComum("produto comum de teste", 1000);
+	}
+
+	@Test
+	public void devePossuirPrecoTotalIgualAoPreco() {
+
+		int precoTotal = produto.getPrecoTotal();
+		int preco = produto.getPreco();
+
+		assertEquals(preco, precoTotal);
+	}
+
+}
