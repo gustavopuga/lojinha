7f4f29754d17bc2da7cf21b8c2fae76af6412eca
Gustavo Fortunato Puga <gustavo.fortunatopuga@gmail.com>
2013-04-13 17:24:22 -0300

    Teste para garantir que o calculo do preço total de um produto manufaturado está correto.

---
+++ src/test/java/br/com/airu/produto/ProdutoManufaturadoTest.java
@@ -0,0 +1,26 @@
+package br.com.airu.produto;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProdutoManufaturadoTest {
+
+	private ProdutoManufaturado produto;
+
+	@Before
+	public void criaProdutoManufaturado() {
+		produto = new ProdutoManufaturado("produto importado de teste", 1000);
+	}
+
+	@Test
+	public void devePossuirPrecoTotalIgualAoPreco() {
+
+		int preco = produto.getPreco();
+		int precoTotal = produto.getPrecoTotal();
+		
+		assertEquals(preco, precoTotal);
+	}
+	
+}
